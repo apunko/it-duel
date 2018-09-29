fb45d478e18bc97e340464df3658e2d59a849e6c
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2018-09-29 13:59:30 +0300

    Load viraables

--- ruby_client.rb
+++ ruby_client.rb
@@ -6,6 +6,7 @@ logger = Logger.new('./log.txt')
 
 require 'rubygems'
 require 'json'
+require 'pry'
 
 Random.srand()
 
@@ -16,15 +17,31 @@ while true
   begin
   	json = JSON.parse(line)
     logger.info("MOVE: #{json}")
+
     move = Random.rand(8)
     if move > 3
       move = move + 1
     end
     dx = move % 3 - 1
     dy = move / 3 - 1
-    puts(JSON.generate([{:rover_id => 1, :action_type => 'move', :dx => dx, :dy => dy}]));
+
+    puts '-' * 100
+    data, base, rovers = get_data(line)
+    puts data
+    puts '*' * 100
+
+    action_type = %w(move dig).sample
+    puts(JSON.generate([{:rover_id => 1, :action_type => action_type, :dx => dx, :dy => dy}]));
   rescue JSON::ParserError
     #don't print to output here, better to STDERR
     STDERR.puts('parse error')
   end
+end
+
+def get_data(line)
+  data = JSON.parse(line)['data']
+  base = data['base']
+  rovers = data['rovers']
+
+  [data, base, rovers]
 end
