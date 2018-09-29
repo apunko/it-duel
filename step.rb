e511c37f67cd8919f378aba4040509a66769205a
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2018-09-29 16:44:25 +0300

    Craters

--- step.rb
+++ step.rb
@@ -24,7 +24,7 @@ class Step
 
   def move_steps_dig_no
      results = []
-     
+
      return results if area.flatten.size == 0
      results << [x - 1, y - 1] unless area.flatten[0] == 5 || area[0][0]['objects'].include?(4)
      results << [x, y - 1] unless area.flatten[1] == 5 || area[0][1]['objects'].include?(4)
@@ -34,6 +34,21 @@ class Step
      results << [x - 1, y + 1] unless area.flatten[6] == 5 || area[2][0]['objects'].include?(4)
      results << [x, y + 1] unless area.flatten[7] == 5 || area[2][1]['objects'].include?(4)
      results << [x + 1, y + 1] unless area[2][2]['objects'].include?(4) || area.flatten[8] == 5
+
+     results
+  end
+
+  def craters
+     results = []
+     return results if area.flatten.size == 0
+     results << [x - 1, y - 1] if area.flatten[0] == 4
+     results << [x, y - 1] if area.flatten[0] == 4
+     results << [x + 1, y - 1] if area.flatten[0] == 4
+     results << [x - 1, y] if area.flatten[0] == 4
+     results << [x + 1, y] if area.flatten[0] == 4
+     results << [x - 1, y + 1] if area.flatten[0] == 4
+     results << [x, y + 1] if area.flatten[0] == 4
+     results << [x + 1, y + 1] if area.flatten[0] == 4
 
      results
   end
